38cb27091c0a4091a798cec6de692f9740214003
Gene Grinberg <Gene@blss-MacBook-Pro.local>
2015-03-12 09:22:13 -0400

    Initial Commit

---
+++ BLSAgenda/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BLSAgenda
+//
+//  Created by Gene Grinberg on 3/12/15.
+//  Copyright (c) 2015 Gene Grinberg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
